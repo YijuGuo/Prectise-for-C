b4682d8c1a20511770e08a98bb668aad6dcb118c
Yiju Guo <yiju.zeroyre@gmail.com>
2021-06-17 01:51:30 +0800

    master

--- Lake counting.cpp
+++ Lake counting.cpp
@@ -28,14 +28,37 @@
 #include <string>
 using namespace std;
 
+void dfs(int i, int j)
+{
+    //将遍历到的位置置为0，则后面深度搜索的时候，不会重复搜索
+    field[i][j]==0;
+
+    // 通过循环遍历8个方向
+    for (int dx = -1; dx <=1; dx++)
+    {
+        for (int dy = -1; dy <= 1; dy++)
+        {
+            int nx = i+dx;
+            int ny = j+dy;
+            if (nx>=0&&nx<N&&ny>=0&&ny<M&&field[nx][ny]==7)
+            {
+                dfs(nx,ny);
+            }   
+        }    
+    }
+    return 0;
+
+}
+
+
 int main()
 {
     int N,M;
     string temp;
     scanf("%d %d",&N,&M);
-    char field[N][M];
 
     int temp_number;
+    int field[N][M];
 
     for(int i = 0;i<N;i++)
     {
@@ -47,7 +70,24 @@ int main()
        }
     }
 
- 
+    //记录深度搜索的次数
+      int res;
 
-    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            //对field随机遍历，寻找水洼“7”，并以此为起点做深度搜索
+            if (field[i][j]== 7)
+            {
+                //深度搜索
+                dfs(i,j);
+                res++;
+            }    
+        }
+        
+    }
+    printf("%d",res);
+
+    return 0;
 }
